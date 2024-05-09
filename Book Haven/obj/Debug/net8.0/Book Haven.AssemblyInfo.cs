03aab94f500894bbcd1ec06feff3dff324b871c9
Eshvara Govender <ST10077567@vcconnect.edu.za>
2024-05-09 09:55:31 +0200

    Program and Migration
    Added code in Program.cs and then migrated this to the database

--- Book Haven/obj/Debug/net8.0/Book Haven.AssemblyInfo.cs
+++ Book Haven/obj/Debug/net8.0/Book Haven.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book Haven")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d224f98aff7bebd4267c95d0e16d61a8b93e2f28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36e75b8de6046b9a74105bc2f8a2f0c2fdeba07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book Haven")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book Haven")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
