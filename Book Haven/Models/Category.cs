f36e75b8de6046b9a74105bc2f8a2f0c2fdeba07
Eshvara Govender <ST10077567@vcconnect.edu.za>
2024-05-09 09:04:53 +0200

    CATEGORY CLASS
    Created a category class under Models and declared variables

---
+++ Book Haven/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Haven.Models
+{
+    public class Category
+    {
+
+        //VARIABLES
+
+        [Key]
+
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
