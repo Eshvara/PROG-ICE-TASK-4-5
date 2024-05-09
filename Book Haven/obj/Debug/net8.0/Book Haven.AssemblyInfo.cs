277e6089cb084d9f6398a185b8a0563229bb270e
Eshvara Govender <ST10077567@vcconnect.edu.za>
2024-05-09 11:48:31 +0200

    Index and Layout
    Added a table in the Index class with Category Name. In Layout, added the design for the Home Page

--- Book Haven/obj/Debug/net8.0/Book Haven.AssemblyInfo.cs
+++ Book Haven/obj/Debug/net8.0/Book Haven.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book Haven")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9f07b660f5392dd9bf570bed21d885ebf07bc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a3f6c71ccd746449fe36e87da6873e06ee7f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book Haven")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book Haven")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
