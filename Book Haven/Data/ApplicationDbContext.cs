b9a3f6c71ccd746449fe36e87da6873e06ee7f51
Eshvara Govender <ST10077567@vcconnect.edu.za>
2024-05-09 10:57:35 +0200

    ApplicationDbContext
    Added a few Book categories in this class and migrated this so these categories now show on SSMS in the Book_Haven database

--- Book Haven/Data/ApplicationDbContext.cs
+++ Book Haven/Data/ApplicationDbContext.cs
@@ -12,5 +12,16 @@ namespace Book_Haven.Data
 
         public DbSet<Category> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Non-fiction", DisplayOrder = 1 },
+                new Category { CategoryId = 2, Name = "Romance", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "Fiction", DisplayOrder = 3 },
+                new Category { CategoryId = 4, Name = "Mystery", DisplayOrder = 4 },
+                new Category { CategoryId = 5, Name = "Fantasy", DisplayOrder = 5 }
+                );
+        }
+
     }
 }
